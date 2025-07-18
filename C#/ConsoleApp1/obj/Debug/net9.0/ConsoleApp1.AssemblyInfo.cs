90e6d472ba48fb619fb9c47fea14c7010843ab2d
Teodor Fredriksson <90504386+TeodorFredriksson95@users.noreply.github.com>
2025-07-19 00:00:38 +0200

    chore: update assembly information and rebuild binaries

--- C#/ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ C#/ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d19db2fe7b6ce968a44757c6bc171a2f781ee8f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92f8051e757eb8898c152f6e11d25505c8466367")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
