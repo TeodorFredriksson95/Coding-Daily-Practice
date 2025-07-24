158ebc6f9cf6d56528c8e7abcf30e0388b3d2429
Teodor Fredriksson <90504386+TeodorFredriksson95@users.noreply.github.com>
2025-07-25 01:48:46 +0200

    Update(folder structure): Updated folder structure to separate Arduino projects between controllers.
    Init(mini controller): Initialized project for second controller (ESP8266)

--- C#/ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ C#/ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f62d1060b6bcd0dc2cc95af8c331fc96ef1fb61")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a1968010514c489287460123731606365bd3831")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
